af3a6a3f7da8fadf486664325f36685ccbf905df
amarruedo <asiermarruedo@gmail.com>
2017-02-01 12:59:23 +0100

    first version

---
+++ var/vaultSecret.groovy
@@ -0,0 +1,39 @@
+#!groovy​
+
+import groovy.json.*
+
+def call(String nodeName = 'master', String vaultAddress = 'http://localhost:8200', String username, String password, String secretName) {
+
+  node(nodeName){
+
+      response = sh(returnStdout: true, script:"set +x; curl -s " + vaultAddress + "/v1/auth/userpass/login/" + username +" -d '{ \"password\": \"" + password + "\" }'").trim()
+      def result = parseJSON(response, "client_token")
+      def vault_token = result.toString()
+
+      secrets = sh(returnStdout: true, script:"set +x; curl -s -X GET -H \"X-Vault-Token:" + vault_token + "\" " + vaultAddress + "/v1/secret/" + secretName).trim()
+      result = parseJSON(secrets, "data")
+
+      return result
+  }
+}
+
+def parseJSON(String response, String secretName){
+
+  def result = new JsonSlurperClassic().parseText(response)
+
+  try {
+
+      if (result.errors)
+          error "Vault: " + result.errors[0].toString()
+      else if (secretName == "client_token" && result.auth.client_token)
+          return result.auth.client_token
+      else if (secretName == "data" && result.data)
+          return result.data
+      else
+          error "Can't retrieve secret"
+  }
+  catch(Exception err)
+  {
+      error err.toString()
+  }
+}
